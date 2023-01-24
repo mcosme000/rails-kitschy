f4fd25938b55a17562a5d1a5ec2bfbf174f277f9
Maria Cosmen <mcosme00@gmail.com>
2023-01-24 21:34:47 +0900

    Create Lesson Policy with scope.all and show? actions

--- app/policies/lesson_policy.rb
+++ app/policies/lesson_policy.rb
@@ -1,8 +1,12 @@
 class LessonPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
   end
 end
