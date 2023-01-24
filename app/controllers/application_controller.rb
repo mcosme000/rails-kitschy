6f072349e976e4aee291a01e630bce14c795b55b
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-01-24 23:24:33 +0900

    updated index view and render it home page too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: :home
   include Pundit::Authorization
 
   # Pundit: allow-list approach
