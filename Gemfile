7f1f09f69b184d5b9940bb690e9853ae8abc3c1e
Mariama KaiKai <mmak.webdev@gmail.com>
2023-01-24 21:20:57 +0900

    database has been seeded with Faker gem

--- Gemfile
+++ Gemfile
@@ -48,7 +48,7 @@ gem "sassc-rails"
 # CUSTOM GEMS
 gem "devise"
 gem "pundit"
-
+gem "faker"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Lesson.destroy_all
+User.destroy_all
+
+puts "Creating a lesson..."
+
+users = []
+
+5.times do
+  users << User.create(
+    email: Faker::Internet.email,
+    password: 1234567
+  )
+end
+
+25.times do
+  Lesson.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    cuisine_genre: Faker::Food.ethnic_category,
+    description: Faker::Food.description,
+    capacity: Faker::Number.within(range: 1..120),
+    fee: Faker::Number.within(range: 0.5..125.0),
+    lesson_length_minutes: Faker::Number.within(range: 45..120),
+    user: users.sample
+    )
+  end
+
+  puts "Finished!"
