671c15997237e95fa7f05af6548fbcb44e9c3b6b
Mariama KaiKai <mmak.webdev@gmail.com>
2023-01-25 11:56:24 +0900

    removed validation notes

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,4 @@ class Lesson < ApplicationRecord
   has_many :bookings
   belongs_to :user
 
-  validate :users, :email, :address, :description, :capacity, :cuisine_genre, :fee, :lesson_length_minutes, presence: true
 end
