75ff8ef11fe34781a278bc11be1b8790a921a27a
Maria Cosmen <mcosme00@gmail.com>
2023-01-25 15:13:33 +0900

    Edit navbar and display links depending on authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :lessons
   has_many :bookings
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
