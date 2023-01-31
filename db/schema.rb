0c6d81072bce83a18f6390ca0ee228da0a64cac6
Jeremy Tan <jeremy.e.tan@gmail.com>
2023-01-31 21:01:00 +0900

    starting work on set URL into Schema. Can be used later for features that allow users to add photo, but for now it will be used to set a string of URL to set images for image_tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_112519) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_115826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_112519) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "main_photo"
     t.index ["user_id"], name: "index_lessons_on_user_id"
   end
 
